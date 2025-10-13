45b8a5ddcfda8810262365c85d3db8b91aea44f3
Anupam Upadhyay <anupamupadhyay@devnexus.in>
2025-10-13 17:38:52 +0530

    2nd update

--- src/Pages/AdminDashboard/Google/GoogleInti.jsx
+++ src/Pages/AdminDashboard/Google/GoogleInti.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react';
+// import { GoogleLogin } from '@react-oauth/google';
+// import axios from 'axios';
 
-const GoogleInti = () => {
-  return (
-    <div>GoogleInti</div>
-  )
-}
+// const GoogleInti = () => {
+//   const [userInfo, setUserInfo] = useState(null);
 
-export default GoogleInti
+//   const handleSuccess = async (response) => {
+//     try {
+//       const { data } = await axios.post('/api/oauth2callback', {
+//         code: response.credential,
+//       });
+//       setUserInfo(data);
+//     } catch (error) {
+//       console.error('Error fetching user info:', error);
+//     }
+//   };
+
+//   const handleError = (error) => {
+//     console.error('Login Failed:', error);
+//   };
+
+//   return (
+//     <div>
+//       {!userInfo ? (
+//         <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+//       ) : (
+//         <div>
+//           <h2>Welcome, {userInfo.email}</h2>
+//           <p>Customer ID: {userInfo.customerId}</p>
+//           {/* Display other Google Ads account details here */}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default GoogleInti;
